ad66b016c509e102f4bccb8f6ac08f5f374fb4fc
fmax <datuyo@gmail.com>
2018-12-15 01:00:07 +0200

    -: added prototype layer editor

---
+++ assets/js/db/map/tile_maps.js
@@ -0,0 +1,39 @@
+var db_tile_maps = [
+	{
+		id: 'tileset_1',
+		class: 'tile-map1',
+		width: 256,
+		height: 336,
+		tileSize: [16, 16]
+	},
+	{
+		id: 'mountains_0',
+		class: 'tile-map2',
+		width: 196,
+		height: 160,
+		tileSize: [16, 16]
+	},
+	{
+		id: 'water_1',
+		class: 'tile-map3',
+		width: 480,
+		height: 32,
+		tileSize: [16, 16],
+		animation: true,
+		frame_time: 1000 // miliseconds
+	},
+    {
+      id: 'cave_1',
+      class: 'tile-map4',
+      width: 256,
+      height: 336,
+      tileSize: [16, 16],
+    },
+    {
+      id: 'sewer_1',
+      class: 'tile-map5',
+      width: 256,
+      height: 336,
+      tileSize: [16, 16],
+    },
+]
--- assets/js/map/config.js
+++ assets/js/map/config.js
@@ -8,45 +8,7 @@ var config = {
 	activeLayer: "",
 	layers: 9,
 	// 256x336 auto tiling by 16px
-	tilesMap: [
-		{
-			id: 'tileset_1',
-			class: 'tile-map1',
-			width: 256,
-			height: 336,
-			tileSize: [16, 16]
-		},
-		{
-			id: 'mountains_0',
-			class: 'tile-map2',
-			width: 196,
-			height: 160,
-			tileSize: [16, 16]
-		},
-		{
-			id: 'water_1',
-			class: 'tile-map3',
-			width: 480,
-			height: 32,
-			tileSize: [16, 16],
-			animation: true,
-			frame_time: 1000 // miliseconds
-		},
-	    {
-	      id: 'cave_1',
-	      class: 'tile-map4',
-	      width: 256,
-	      height: 336,
-	      tileSize: [16, 16],
-	    },
-	    {
-	      id: 'sewer_1',
-	      class: 'tile-map5',
-	      width: 256,
-	      height: 336,
-	      tileSize: [16, 16],
-	    },
-	],
+	tilesMap: db_tile_maps,
 	tiles: [],
 	map_list: [db_map_zone1, db_map_sewer1, db_map_sewer1_b1],
 	activeMapId: null,
