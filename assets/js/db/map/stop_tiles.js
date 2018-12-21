43744c881b0b453e23d01493813d398ef7d51f56
fmax <datuyo@gmail.com>
2018-12-21 17:44:23 +0200

    -: partially added stop tiles for cave map

--- assets/js/db/map/stop_tiles.js
+++ assets/js/db/map/stop_tiles.js
@@ -4,7 +4,16 @@ var stop_tiles = {
 			"tile-map1_2_2", "tile-map1_3_0", "tile-map1_3_1", "tile-map1_4_0", "tile-map1_4_1", "tile-map1_10_13", "tile-map1_10_15",
 			"tile-map1_14_13", "tile-map1_14_15", "tile-map1_9_0", "tile-map1_9_1", "tile-map1_8_0", "tile-map1_8_1"
 		],
-		"tile-map4": [],
+		"tile-map4": [
+			"tile-map4_0_0", "tile-map4_0_1", "tile-map4_0_2",
+			"tile-map4_8_6", "tile-map4_8_7",
+			"tile-map4_9_6", "tile-map4_9_7",
+			"tile-map4_9_2", "tile-map4_8_2", "tile-map4_7_2",
+			"tile-map4_5_3", "tile-map4_5_4", "tile-map4_5_5",
+			"tile-map4_2_12", "tile-map4_2_13", "tile-map4_2_14", "tile-map4_2_15",
+			"tile-map4_5_6", "tile-map4_5_7", "tile-map4_5_8",
+			"tile-map4_6_6", "tile-map4_6_8", "tile-map4_7_6", "tile-map4_7_7", "tile-map4_7_8",
+		],
 		"tile-map5": [
 			"tile-map5_3_0", "tile-map5_3_1", "tile-map5_3_2", "tile-map5_3_7",
 			"tile-map5_3_8", "tile-map5_3_9", "tile-map5_4_0", "tile-map5_4_1",
